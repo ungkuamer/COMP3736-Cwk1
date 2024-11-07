a991266ea47249cfdfd9fe1a3ac22c2f35793927
Ungku Amer Ungku Aziz <ungkuamer@gmail.com>
2024-11-07 11:33:19 +0000

    feat: combine data on same page with two plots and cleanup

--- app/views.py
+++ app/views.py
@@ -14,73 +14,45 @@ matplotlib.use('Agg')
 def home():
     return render_template('index.html')
 
-@app.route('/scatter')
-def scatter():
-
+@app.route('/answer')
+def answer():
     school_data = generate_random_data()
     
     months = list(range(1, 13))
+    months_name = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    schools_name = ["School " + str(i) for i in range(10)]
 
+    # Scatter Plot
     plt.figure(figsize=(10, 6))
     for i, school in enumerate(school_data):
         plt.scatter(months, school, label=f'Sc {i}', alpha=0.7)
     plt.xlabel("Month")
     plt.ylabel("Random Value")
     plt.title("Scatter Plot of Random Data for Schools Across Months")
-    plt.xticks(months, ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+    plt.xticks(months, months_name)
     plt.legend(loc="upper right", bbox_to_anchor=(1.15, 1))
 
-    # Save plot to an in-memory image
     buf = io.BytesIO()
-    plt.savefig(buf, format='png')
+    plt.savefig(buf, format='png', bbox_inches='tight')
     buf.seek(0)
-    img_base64 = base64.b64encode(buf.getvalue()).decode('utf8')
-    plt.close()  # Close the plot to free up memory
+    img1 = base64.b64encode(buf.getvalue()).decode('utf8')
+    plt.close()
 
-    # Render HTML with embedded image
-    html = f"""
-    <html>
-    <body>
-        <h1>Scatter Plot of Random Data</h1>
-        <img src="data:image/png;base64,{img_base64}" alt="Scatter Plot"/>
-    </body>
-    </html>
-    """
-    return render_template_string(html)
-
-@app.route('/heatmap')
-def heatmap():
-
-    school_data = np.array(generate_random_data())
-    schools_name = ["School " + str(i) for i in range(10)]
-    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-    # Create the heatmap
+    # Heatmap
     plt.figure(figsize=(8, 6))
     plt.imshow(school_data, cmap="viridis", aspect="auto")
     for i in range(len(schools_name)):
         for j in range(len(months)):
-            text = plt.text(j, i, school_data[i][j], ha="center", va="center", color="w")
+            plt.text(j, i, school_data[i][j], ha="center", va="center", color="w")
 
-    # Set ticks and labels
-    plt.xticks(ticks=np.arange(len(months)), labels=months, rotation=45)
+    plt.xticks(ticks=np.arange(len(months)), labels=months_name, rotation=45)
     plt.yticks(ticks=np.arange(len(schools_name)), labels=schools_name)
     plt.title("Heatmap of Random Data for Schools Across Months")
     
-    # Save the plot to a BytesIO object
     buf = io.BytesIO()
-    plt.savefig(buf, format='png')
+    plt.savefig(buf, format='png', bbox_inches='tight')
     buf.seek(0)
-    img_base64 = base64.b64encode(buf.getvalue()).decode('utf8')
-    plt.close()  # Close the plot to free up memory
+    img2 = base64.b64encode(buf.getvalue()).decode('utf8')
+    plt.close()
 
-    # Embed the image in HTML
-    html = f"""
-    <html>
-    <body>
-        <h1>Heatmap of Random Data for Schools</h1>
-        <img src="data:image/png;base64,{img_base64}" alt="Heatmap"/>
-    </body>
-    </html>
-    """
-    return render_template_string(html)
+    return render_template('answer.html', plot1=img1, plot2=img2)
